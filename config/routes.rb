baf21deeaa4ef3e929a5bf3eabf858f632c5dfd0
Gabriel Boucher <gboucher@stingray.com>
2020-09-02 11:37:19 -0400

    chapter 7: add signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "about"     => "static_pages#about"
   get "contact"   => "static_pages#contact"
   get "singup"    => "users#new"
+
+  resources :users
   
 end
